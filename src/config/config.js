b8a19ea9b5b20652d195a4bbbbc3e4037c691857
S <ap311036@gmail.com>
2018-11-02 16:54:38 +0800

    Updates

--- src/config/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    apiKey: "AIzaSyDJtTHcZ7cb4ZgJ02AszFPogHTMULv14_Q",
-    authDomain: "reactuse.firebaseapp.com",
-    databaseURL: "https://reactuse.firebaseio.com",
-    projectId: "reactuse",
-    storageBucket: "reactuse.appspot.com",
-    messagingSenderId: "808054206342"
-};
-export default config;
